b08e23a0c2de684c6c7eed05627b3a7f32271411
Celiken <eowilisean@gmail.com>
2023-05-02 19:14:59 +0200

    Add starry background
    customizable star shader

---
+++ Assets/Scripts/FollowUV.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowUV : MonoBehaviour
+{
+    [SerializeField] private float OverallScale;
+    [SerializeField] private Color Tint;
+    [SerializeField] private float ClipThreshold;
+    [SerializeField] private float StarsScale;
+    [SerializeField] private float Randomness;
+    [SerializeField] private float BrightnessVariationScale;
+    [SerializeField] private float BrightnessPower;
+    [SerializeField] private float Brightness;
+
+
+    [SerializeField] private float parralax = 1;
+
+    private SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        sr.material.SetColor("_Tint", Tint);
+        sr.material.SetFloat("_OverallScale", OverallScale);
+        sr.material.SetFloat("_ClipThreshold", ClipThreshold);
+        sr.material.SetFloat("_StarsScale", StarsScale);
+        sr.material.SetFloat("_Randomness", Randomness);
+        sr.material.SetFloat("_BrightnessVariationScale", BrightnessVariationScale);
+        sr.material.SetFloat("_BrightnessPower", BrightnessPower);
+        sr.material.SetFloat("_Brightness", Brightness);
+    }
+
+    void Update()
+    {
+        Vector2 offset;
+        offset.x = transform.position.x / transform.localScale.x / parralax;
+        offset.y = transform.position.z / transform.localScale.y / parralax;
+        sr.material.SetVector("_Offset", offset);
+    }
+}
